4e9d6c77d91df1dc3968eaec0efe6ca584b454f4
lggradosm <gabrielgradosmachado@gmail.com>
2016-05-24 08:27:48 -0500

    Update Principal.java
    Prueba de la actualización mediante la página.

--- AplicacionEmtrafesa/src/aplicacionemtrafesa/Principal.java
+++ AplicacionEmtrafesa/src/aplicacionemtrafesa/Principal.java
@@ -15,7 +15,8 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        FormPrincipal p = new FormPrincipal();
+        p.setVisible(true);
     }
     
 }
